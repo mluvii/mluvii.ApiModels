d58a38f990b193e1b35824a6dca0ad7325dc7f54
martin.prazak <martin.prazak@mluvii.com>
2021-01-07 12:03:36 +0100

    M2-8365 extended model for tags rest api

---
+++ Tags/TagModel.cs
@@ -0,0 +1,17 @@
+﻿namespace mluvii.ApiModels.Tags
+{
+    public class TagModel
+    {
+        public int Id { get; set; }
+
+        public string Name { get; set; }
+
+        public string Label { get; set; }
+
+        public int DepartmentId { get; set; }
+
+        public uint BackgroundArgb { get; set; }
+
+        public uint TextArgb { get; set; }
+    }
+}
