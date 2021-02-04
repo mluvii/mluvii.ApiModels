6e4dfb426a3a60e4b0fe196e4e22a400df653d5f
martin.prazak <martin.prazak@mluvii.com>
2021-02-04 15:09:36 +0100

    M3-146 added campaign model

--- Campaigns/CampaignCreateModel.cs
+++ Campaigns/CampaignCreateModel.cs
@@ -3,15 +3,9 @@ using System.ComponentModel.DataAnnotations;
 
 namespace mluvii.ApiModels.Campaigns
 {
-    public class CampaignCreateModel
+    public class CampaignBaseModel
     {
         public string Name { get; set; }
-
-        [EnumDataType(typeof(Channel))]
-        public Channel Channel { get; set; }
-
-        [EnumDataType(typeof(CampaignMode))]
-        public CampaignMode Mode { get; set; }
 
         public int? PickupRate { get; set; }
 
@@ -24,9 +18,20 @@ namespace mluvii.ApiModels.Campaigns
         public int? RuleId { get; set; }
 
         public int? BcwSeconds { get; set; }
+
+        public string[] CallerIds { get; set; }
     }
 
-    public class CampaignUpdateModel : CampaignCreateModel
+    public class CampaignCreateModel : CampaignBaseModel
+    {
+        [EnumDataType(typeof(Channel))]
+        public Channel Channel { get; set; }
+
+        [EnumDataType(typeof(CampaignMode))]
+        public CampaignMode Mode { get; set; }
+    }
+
+    public class CampaignUpdateModel : CampaignBaseModel
     {
         public int DepartmentId { get; set; }
     }
