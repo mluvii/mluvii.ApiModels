dca83e7da150ac2bda47c55a509f9a213726abe9
martin.prazak <martin.prazak@mluvii.com>
2021-06-23 13:40:23 +0200

    M3-1074 added CampaignIdentity and CampaignIdentityState

---
+++ Campaigns/CampaignIdentity.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace mluvii.ApiModels.Campaigns
+{
+    public class CampaignIdentity
+    {
+        public long Id { get; set; }
+
+        [EnumDataType(typeof(CampaignIdentityState))]
+        public CampaignIdentityState State { get; set; }
+
+        public DateTime? Created { get; set; }
+
+        public DateTime? RetryScheduledDate { get; set; }
+
+        public int RetryCount { get; set; }
+    }
+}
