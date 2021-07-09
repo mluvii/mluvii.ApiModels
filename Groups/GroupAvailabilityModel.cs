ce0e1073e3a2ebed6a12d8c15f85975e9dcdbc1e
martin.prazak <martin.prazak@mluvii.com>
2021-07-09 11:13:27 +0200

    M3-1236 added GroupAvailabilityModel

---
+++ Groups/GroupAvailabilityModel.cs
@@ -0,0 +1,28 @@
+﻿using mluvii.ApiModels.Common;
+using System.Collections.Generic;
+
+namespace mluvii.ApiModels.Groups
+{
+    public class GroupAvailabilityModel
+    {
+        public Dictionary<int, GroupOverallWorkload> OverallWorkload { get; set; }
+    }
+
+    public class GroupOverallWorkload
+    {
+        public Dictionary<Channel, GroupWorkload> Workload { get; set; }
+    }
+
+    public class GroupWorkload
+    {
+        public int InQueueCountTotal { get; set; }
+
+        /// <summary>
+        /// Count of used shared slots
+        /// It is used in whole group routing
+        /// </summary>
+        public int SharedSlotsUsedTotal { get; set; }
+
+        public int FreeQueueSlotsCountTotal { get; set; }
+    }
+}
