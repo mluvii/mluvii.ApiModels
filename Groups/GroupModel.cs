782553716a30aa9c6632f1a95975c67d259a25e0
martin.prazak <martin.prazak@mluvii.com>
2021-08-11 09:38:22 +0200

    M3-1404 group model

---
+++ Groups/GroupModel.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace mluvii.ApiModels.Groups
+{
+    public class GroupModel
+    {
+        public int Id { get; set; }
+
+        public string Name { get; set; }
+
+        public DateTime Created { get; set; }
+
+        public OperatorGroupType Type { get; set; }
+
+        public int DepartmentId { get; set; }
+
+    }
+}
