88bc172fb532ef000c8506d769a96918cb5d6e2c
martin.prazak <martin.prazak@mluvii.com>
2021-08-11 09:38:22 +0200

    M3-1404 group model

---
+++ Groups/OperatorGroupType.cs
@@ -0,0 +1,10 @@
+﻿namespace mluvii.ApiModels.Groups
+{
+    public enum OperatorGroupType
+    {
+        UNKNOWN = 0,
+        NORMAL = 1,
+        ALL_OPERATORS = 2,
+        CHATBOT = 3
+    }
+}
