40000f620427db440317f811e2c561f6a0791824
martin.prazak <martin.prazak@mluvii.com>
2021-11-19 09:35:29 +0100

    numeric enum values should not be used when working with public api

--- Common/InteractionParamSource.cs
+++ Common/InteractionParamSource.cs
@@ -3,15 +3,15 @@
     public enum InteractionParamSource
     {
         UNKNOWN = 0,
-        User = 1,
-        Chatbot = 2,
-        HeroCard = 3,
-        EntryForm = 4,
-        FeedbackForm = 5,
-        OfflineForm = 6,
-        ExternalMessaging = 7,
-        Phone = 8,
+        User,
+        Chatbot,
+        HeroCard,
+        EntryForm,
+        FeedbackForm,
+        OfflineForm,
+        ExternalMessaging,
+        Phone,
         // ReSharper disable once InconsistentNaming -- it is an abbreviation
-        API = 9
+        API
     }
 }
