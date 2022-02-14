1ac405d42e00c451322e187a9287f7b7ad53ed0e
Ondřej Vondra <ondrej.vondra@icord.cz>
2022-02-14 13:10:32 +0100

    OutboundCallModel

---
+++ Phones/OutboundCallModel.cs
@@ -0,0 +1,18 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace mluvii.ApiModels.Phones
+{
+    public class OutboundCallModel
+    {
+        [Required]
+        public int OperatorId { get; set; }
+
+        [Required]
+        public int DepartmentId { get; set; }
+
+        [Required]
+        public string PhoneNumber { get; set; }
+
+        public string CallerId { get; set; }
+    }
+}
