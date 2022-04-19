7fbd23c1fc7d239532aa2d38dcffb7d197353eb0
Petr Castek <petr.castek@gmail.com>
2022-04-19 18:11:27 +0200

    M3-2835 asterisk events

---
+++ Phones/PhoneCall.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace mluvii.ApiModels.Phones
+{
+    public class PhoneCall
+    {
+        public long PhoneCallId { get; set; }
+        public string UniqueId { get; set; }
+        public int CompanyDepartmentId { get; set; }
+        public DateTimeOffset Created { get; set; }
+        public IList<PhoneCallEvent> Events { get; set; }
+        public long? SessionId { get; set; }
+        public string From { get; set; }
+        public string To { get; set; }
+    }
+}
---
+++ Phones/PhoneCallEvent.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace mluvii.ApiModels.Phones
+{
+    public class PhoneCallEvent
+    {
+        public string Event { get; set; }
+        public DateTime Date { get; set; }
+        public IDictionary<string, string> Action { get; set; }
+    }
+}
