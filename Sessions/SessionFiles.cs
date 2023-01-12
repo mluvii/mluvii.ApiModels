bbf8ba2d60e0ad4bc6c3ebea60cba5860b6c12cd
Petr Castek <petr.castek@gmail.com>
2023-01-12 14:18:42 +0100

    M3-3538 file removal

---
+++ Sessions/SessionFiles.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections.Generic;
+
+namespace mluvii.ApiModels.Sessions
+{
+    public class SessionFiles
+    {
+        public ICollection<int> FileIds { get; set; }
+    }
+
+    public enum SessionFileRemovalState
+    {
+        UNKNOWN = 0,
+        REMOVED = 1,
+        ALREADY_REMOVED = 2,
+        INACCESSIBLE = 4,
+    }
+
+    public class SessionFileResponse
+    {
+        public int FileId { get; set; }
+        public SessionFileRemovalState State { get; set; }
+    }
+
+}
