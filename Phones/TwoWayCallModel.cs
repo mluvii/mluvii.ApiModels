cf41d488f15245c4ffad133c398d00333450ea68
ofogatos <ondrej.fogatos@mluvii.com>
2024-01-23 17:05:21 +0100

    M3-4977 two way call model added

---
+++ Phones/TwoWayCallModel.cs
@@ -0,0 +1,31 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace mluvii.ApiModels.Phones
+{
+    public class TwoWayCallModel
+    {
+        /// <summary>
+        /// Id of the tenant in which the phone session is to be created.
+        /// </summary>
+        [Required]
+        public int DepartmentId { get; set; }
+
+        /// <summary>
+        /// First number to be dialed. This should be the agent/operator.
+        /// </summary>
+        [Required]
+        public string FirstNumber { get; set; }
+
+        /// <summary>
+        /// Second number to be dialed. This should be the client.
+        /// </summary>
+        [Required]
+        public string SecondNumber { get; set; }
+
+        /// <summary>
+        /// Number to be displayed on the client's device when they receive the call.
+        /// </summary>
+        [Required]
+        public string MaskNumber { get; set; }
+    }
+}
