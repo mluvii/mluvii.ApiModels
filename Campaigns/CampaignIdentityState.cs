671dfb2df652ba0c77112ab8653d1d0a17175f40
Ondřej Vondra <ondrej.vondra@icord.cz>
2024-04-29 14:27:48 +0200

    missing campaign identity states

--- Campaigns/CampaignIdentityState.cs
+++ Campaigns/CampaignIdentityState.cs
@@ -11,12 +11,19 @@
         CALLING,
         CALL_IN_PROGRESS,
         SUCCESS,
+        FAILING,
         DIAL_FAILED,
         RETRY_SCHEDULED,
+        WA_SENT,
+        WA_SEND_FAILED,
+        WA_DELIVERED,
+        WA_DELIVERY_FAILED,
+        WA_READ,
         CANCELLED_BY_ADMIN,
         CANCELLED_BY_CLIENT,
         CANCELLED_BY_OPERATOR,
         BLACKLISTED,
-        INVALID_RECORD
+        INVALID_RECORD,
+        REMOVED
     }
 }
