00e6388a8785d372d48372769e7b5a2049578d22
Ondřej Vondra <ondrej.vondra@icord.cz>
2025-08-15 13:23:17 +0200

    assign contact id when creating email

--- Emails/EmailThreadCreateModel.cs
+++ Emails/EmailThreadCreateModel.cs
@@ -1,5 +1,6 @@
 using System;
 using System.Collections.Generic;
+using System.ComponentModel;
 using System.ComponentModel.DataAnnotations;
 
 namespace mluvii.ApiModels.Emails
@@ -12,7 +13,14 @@ namespace mluvii.ApiModels.Emails
         /// <summary>
         /// If set, the thread will be assigned to the given operator for manual processing.
         /// </summary>
+        [DefaultValue(null)]
         public int? OperatorUserId { get; set; }
+
+        /// <summary>
+        /// If set, a contact directory contact will be assigned to the thread.
+        /// </summary>
+        [DefaultValue(null)]
+        public long? ContactId { get; set; }
 
         /// <summary>
         /// Use <see cref="ClientEmails"/> instead.
