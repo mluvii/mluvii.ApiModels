c7673be34b440b0278aebb95678c9a50c7c954fc
Ondřej Vondra <ondrej.vondra@icord.cz>
2026-04-07 11:25:55 +0200

    fixed UpdateUserModel by removing unsupported OpenIDProvider property

--- Users/UpdateUserModel.cs
+++ Users/UpdateUserModel.cs
@@ -1,6 +1,19 @@
+using System.ComponentModel.DataAnnotations;
+
 namespace mluvii.ApiModels.Users
 {
-    public class UpdateUserModel : CreateUserModel
+    public class UpdateUserModel
     {
+        [Required]
+        public string FirstName { get; set; }
+
+        [Required]
+        public string LastName { get; set; }
+
+        [Required]
+        public string Username { get; set; }
+
+        [Required]
+        public string Email { get; set; }
     }
 }
